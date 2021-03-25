b32861d3c26782c9aa724b2712a47252a852e1d1
KShekhurin <k.shekhurin@gmail.com>
2021-03-25 23:14:21 +0300

    Remake main game class for working with controllers. Added game preferences class.

--- game.py
+++ game.py
@@ -1,16 +1,27 @@
 import pygame
 import sys
 
-class Game:
+
+class Game_prefs:
     def __init__(self, size, frame_rate, name):
-        self.stop = False
         self.size = size
         self.FPS = frame_rate
         self.name = name
-        self.objects = []
-        
+
+
+class Game:
+    def __init__(self, preferences: Game_prefs, controllers, start_route):
         self.state = dict() 
+        self.state['route'] = start_route
+
+        self.controllers = map(
+            lambda controller: controller.connect_state(self.state),
+            controllers
+        )
         
+        self.stop = False
+
+        #I'll try to make it not depending on exactly pygame (in future)
         pygame.init()
         pygame.display.set_caption(self.name)
         self.screen = pygame.display.set_mode(self.size)
@@ -21,23 +32,16 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
-    def update(self, events):
-        for obj in self.objects:
-            obj.update(events)
-    
-    def draw(self):
-        for obj in self.objects:
-            obj.draw(self.screen)
     
     def start(self):
         while not self.stop:
             self.clock.tick(self.FPS)
-            self.screen.fill((0, 0, 0))
+
+            controller_route = state['start_route']
 
             events = pygame.event.get()
-
             self.check_special_events(events)
-            self.update(events)
-            self.draw()
+
+            controllers[controller_route].update(events)
+            controllers[controller_route].draw()
 
