e59ecb4e3e639f9cd7f82b0539765b38cb2dde13
KShekhurin <k.shekhurin@gmail.com>
2021-03-26 14:57:41 +0300

    renamed to controllers and added contollers manager.

--- controllers.py
+++ controllers.py
@@ -1,3 +1,9 @@
+class Controllers_manager:
+    def __init__(self, controllers_with_routes, start_route):
+        self.controllers_with_routes = controllers_with_routes
+        self.start_route = start_route
+
+
 class Controller_base:
     def __init__(self):
         self.state = dict()
